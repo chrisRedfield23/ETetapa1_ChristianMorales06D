928bdc1207e23c906475632aeb1b357d03f40e1a
Christian Morales M <christian.pancake771@gmail.com>
2021-07-02 18:01:02 -0400

    2. Implementación de oracle, creación de usuario para django

--- ETetapa1_ChristianMorales006D/caos/models.py
+++ ETetapa1_ChristianMorales006D/caos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class colaboradores(models.Model):
+    rut = models.CharField(max_length=40,primary_key=True, verbose_name='rut: ')
+    foto=models.ImageField(upload_to='img/', null=True, blank=True)
+    nombre  = models.CharField(max_length=20, verbose_name='nombre')
+    apellido = models.CharField(max_length=20, verbose_name='apellido')
+    telefono  = models.IntegerField(verbose_name='telefono')
+    direccion = models.CharField(max_length=50, verbose_name='direccion')
+    correo_electronico = models.CharField(max_length=40, verbose_name='correo_electronico')
+    pais  = models.CharField(max_length=15, verbose_name='pais')
+    contrasena = models.CharField(max_length=20, verbose_name='contrasena')
+
+    def _str_(self):
+        return(self.rut)
